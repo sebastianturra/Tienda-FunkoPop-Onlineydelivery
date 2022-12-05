a3e3da8606094ee9107aa89af15ca97d33a0c9c1
Sebastián <sebastianturra@gmail.com>
2022-12-05 20:23:28 -0300

    Carga 5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*
 Indice
-0. Declaraciones 
+0. Declaraciones y localStorage
 1. Clase
 2. Array
 3. Variables
@@ -9,15 +9,22 @@ Indice
 6. Main
 */
 
-/*0. Declaraciones
+/*0. Declaraciones y localStorage
 ---------------------------------------------------------------------*/
+//Declaraciones
 let contenedorHTML = document.getElementById('contenedorIndex');
 let filtroHTML = document.getElementById('filtroIndex');
 let productosHTML = document.getElementById('productosIndex');
 let publicidadHTML = document.getElementById('publicidadIndex');
 let inputSearch = document.getElementById('input-search');
 let contenedorCarrito = document.getElementById('contenedorCarrito');
-                       
+
+//localStorage y sessionStorage
+const carritoStorageVisita = 'carritoVisita';
+const carritoStorageMasTarde = 'carritoVisitaMasTarde';
+const listaProductos = 'listaProductos';
+
+
 /*1. Clase
 ---------------------------------------------------------------------*/
 class Funko{
@@ -42,8 +49,67 @@ class Carrito{
 
 /*2. Array 
 ---------------------------------------------------------------------*/
-let productosFunkos = [];
 let carritoFunkos = [];
+
+let productosFunkos = [{
+    id:1,
+    nombre: "America Chavez",
+    valor: 99999,
+    cantidad: 99,
+    imagen: 'img/productos/AmericaChavez.webp'
+},
+{
+    id:2,
+    nombre: "Bb8",
+    valor: 11111,
+    cantidad: 11,
+    imagen: 'img/productos/Bb8.webp'
+},
+{
+    id:3,
+    nombre: "Dr. Strange",
+    valor: 22222,
+    cantidad: 22,
+    imagen: 'img/productos/DrStrange.webp'
+},
+{
+    id:4,
+    nombre: "Therizinosaurus",
+    valor: 33333,
+    cantidad: 33,
+    imagen: 'img/productos/Therizinosaurus.webp'
+},
+{
+    id:5,
+    nombre: "La cenisienta",
+    valor: 44444,
+    cantidad: 44,
+    imagen: 'img/productos/Lacenisienta.webp'
+},
+{
+    id:6,
+    nombre: "Laroca",
+    valor: 55555,
+    cantidad: 55,
+    imagen: 'img/productos/Laroca.webp'
+},
+{
+    id:7,
+    nombre: "Rocky",
+    valor: 66666,
+    cantidad: 66,
+    imagen: 'img/productos/Rocky.webp'
+},
+{
+    id:8,
+    nombre: "Sulley",
+    valor: 77777,
+    cantidad: 77,
+    imagen: 'img/productos/Sulley.webp'
+},
+];
+
+sessionStorage.setItem(listaProductos,JSON.stringify(productosFunkos));
 
 /*3. Variables default 
 -------------------------------------------------------------------- */
@@ -142,10 +208,16 @@ let total = () => {
 }
 */
 
-let mostrarTodosFunkos = (array,contenedor) => {    
+let mostrarTodosFunkos = (array,contenedor) => {   
+
+    let arrayFunkos = JSON.parse(sessionStorage.getItem(array));
+    if(!arrayFunkos){
+        arrayFunkos = array;
+    }
+
     let stringFunko = '';
 
-    array.forEach((producto) => {
+    arrayFunkos.forEach((producto) => {
         stringFunko += `
         <div class="card" style="width: 12rem;">
             <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
@@ -163,52 +235,52 @@ let mostrarTodosFunkos = (array,contenedor) => {
 
 /* Funciones clase carrito *
 ---------------------------------------------------------------------------------------------*/
-let agregarCarrito = (id) => {
+const agregarCarrito = (id) => {
+    
+    const arrayFunkos = JSON.parse(sessionStorage.getItem(listaProductos));
+    const carroVisita = JSON.parse(localStorage.getItem(carritoStorageVisita));
+
     if(!id){
         window.alert('id no definida');
         console.log('id no definida');
         return
     }
 
-    const producto = productosFunkos.find((el) => el.id === id);
+    const producto = arrayFunkos.find((el) => el.id === id);
 
     if(producto){
         let funkoCarrito = new Carrito(producto.id,producto.nombre,producto.valor,1);
 
-            if(carritoFunkos.some((el) => el.id === id)){
-               const funkoFind = carritoFunkos.find((el) => el.id === id);
-               carritoFunkos = carritoFunkos.filter((el) => el.id !== funkoFind.id);
-               carritoFunkos.push(new Carrito(funkoFind.id,funkoFind.nombre,funkoFind.valor,funkoFind.cantidad+1));
-    
-            }else{
+            if(!carroVisita){
                 carritoFunkos.push(funkoCarrito); 
-    
+                localStorage.setItem(carritoStorageVisita,JSON.stringify(carritoFunkos));
+
+            }else{
+                if(carroVisita.some((el) => el.id === id)){
+                    const funkoFind = carroVisita.find((el) => el.id === id);
+                    carritoFunkos = carroVisita.filter((el) => el.id !== funkoFind.id);
+                    carritoFunkos.push(new Carrito(funkoFind.id,funkoFind.nombre,funkoFind.valor,funkoFind.cantidad+1));
+     
+                    localStorage.setItem(carritoStorageVisita,JSON.stringify(carritoFunkos));
+
+                 }else{
+                     carritoFunkos.push(funkoCarrito); 
+                     localStorage.setItem(carritoStorageVisita,JSON.stringify(carritoFunkos));
+                 }
             }
     }   
     console.log(carritoFunkos);
+    console.log(JSON.parse(localStorage.getItem(carritoStorageVisita)));
 };
 
-let busqueda = () => {
-    
-}
+const mostrarCarrito = () => {
 
-let elimminar = () => {
-    
-}
+    let carritoVisitaLocal = JSON.parse(localStorage.getItem(carritoStorageVisita));
 
-let modificar = () => {
-    
-}
-
-let totalCarrito = () =>{
-
-}
-
-let mostrarCarrito = () => {
     let acumulador = '';
     let iterador = 1;
 
-    carritoFunkos.forEach((el) => {
+    carritoVisitaLocal.forEach((el) => {
         acumulador += `
         <tr>
         <td>${iterador}</td>
@@ -217,28 +289,102 @@ let mostrarCarrito = () => {
         <td>${el.cantidad }</td>
         <td>${el.valor } </td>
         <td>${el.valor*el.cantidad}</td>
-        <td>Eliminar(id)|masTarde(id))</td>
+        <td><button onclick='eliminar(${el.id})'><i class="fa-solid fa-trash"></i></button>|<button onclick='masTarde(${el.id})'>
+        <i class="fa-regular fa-clock"></i></button></td>
         </tr>
         `
         iterador++;
     });
-
+    
     contenedorCarrito.innerHTML = acumulador;
+}
+
+const eliminar = (id) => {
+    const carroVisita = JSON.parse(localStorage.getItem(carritoStorageVisita))
+
+    const funkoEncontrado = carroVisita.find((el) => el.id === id);
+    
+    if(funkoEncontrado){
+        
+        let indexFunko = carroVisita.findIndex((el) => el.id === id);
+        
+        if(carroVisita[indexFunko].cantidad === 0){
+            carroVisita.splice(indexFunko,1);
+            localStorage.setItem(carritoStorageVisita,JSON.stringify(carroVisita));
+            mostrarCarrito();
+            return
+        }
+        
+        carroVisita[indexFunko].cantidad-= 1; 
+        
+        if(carroVisita[indexFunko].cantidad === 0){
+            carroVisita.splice(indexFunko,1); 
+            localStorage.setItem(carritoStorageVisita,JSON.stringify(carroVisita));
+            mostrarCarrito();
+        }else{
+            localStorage.setItem(carritoStorageVisita,JSON.stringify(carroVisita));
+            mostrarCarrito();
+        }
+        
+        limpiarArrayLocalStorage(carritoStorageVisita);
+    }
+}
+
+const limpiarArrayLocalStorage = (array) => {
+    let arrayExtraido = JSON.parse(localStorage.getItem(array));
+
+    if(!arrayExtraido.length){
+        localStorage.removeItem(array);
+        carritoFunkos = [];
+    }
+}
+
+const arrayVisitaMasTarde = () => {}
+/*
+const masTarde = (id) => {
+
+    const carroVisita = JSON.parse(localStorage.getItem(carritoStorageVisita));
+    const carromasTarde = JSON.parse(localStorage.getItem(carritoStorageMasTarde));
+
+    //Encuentro index
+    let indexFunko = carroVisita.findIndex((el) => el.id === id);
+
+    //Extraigo el funko deseado
+    let sliceFunko = carritoFunkos.slice(indexFunko,(indexFunko+1));
+
+    //Creo un array con sin el funko desaeado
+    const filterFunko = carroVisita.filter((el) => el.id !== sliceFunko[0].id);
+    carritoFunkos = filterFunko;
+    
+    localStorage.setItem(carritoStorageVisita,JSON.stringify(filterFunko));
+
+    mostrarCarrito();
+        
+}
+*/
+const busqueda = () => {
+}
+    
+const modificar = () => {
+    
+}
+
+const totalCarrito = () =>{
+
 }
 
 /*5. Eventos
 ----------------------------------------------------------------------*/
 let handlerEvent = (e) =>{
-
+    const arrayFunkosEvent = JSON.parse(sessionStorage.getItem(listaProductos));
     /* isNan(){
     e.preventdefault();
     }
     */
     
     const filtrado = e.target.value;
-    console.log(filtrado);
 
-    const array = productosFunkos.filter((producto) => producto.nombre.includes(capitalizar(filtrado.toLowerCase().trim())))
+    let array = arrayFunkosEvent.filter((producto) => producto.nombre.toLowerCase().includes(filtrado.toLowerCase().trim()));
 
     mostrarTodosFunkos(array,productosHTML);
 }
@@ -275,62 +421,7 @@ do{
 }while(accion !== '0' );
 */
 
-productosFunkos = [{
-    id:1,
-    nombre: "America Chavez",
-    valor: 99999,
-    cantidad: 99,
-    imagen: 'img/productos/AmericaChavez.webp'
-},
-{
-    id:2,
-    nombre: "Bb8",
-    valor: 11111,
-    cantidad: 11,
-    imagen: 'img/productos/Bb8.webp'
-},
-{
-    id:3,
-    nombre: "Dr. Strange",
-    valor: 22222,
-    cantidad: 22,
-    imagen: 'img/productos/DrStrange.webp'
-},
-{
-    id:4,
-    nombre: "Therizinosaurus",
-    valor: 33333,
-    cantidad: 33,
-    imagen: 'img/productos/Therizinosaurus.webp'
-},
-{
-    id:5,
-    nombre: "La cenisienta",
-    valor: 44444,
-    cantidad: 44,
-    imagen: 'img/productos/Lacenisienta.webp'
-},
-{
-    id:6,
-    nombre: "Laroca",
-    valor: 55555,
-    cantidad: 55,
-    imagen: 'img/productos/Laroca.webp'
-},
-{
-    id:7,
-    nombre: "Rocky",
-    valor: 66666,
-    cantidad: 66,
-    imagen: 'img/productos/Rocky.webp'
-},
-{
-    id:8,
-    nombre: "Sulley",
-    valor: 66666,
-    cantidad: 66,
-    imagen: 'img/productos/Sulley.webp'
-},
-];
+/* Poner un localstorage
+--------------------------------------------------------------------------------*/
 
-mostrarTodosFunkos(productosFunkos,productosHTML)
+mostrarTodosFunkos(listaProductos,productosHTML);
